1a88c42e64fad7e258332d1bd690896fc21e4f8f
AndreiVatavu <andrei.vatavu097@gmail.com>
2019-04-27 18:53:43 +0300

    Login Page refactored

---
+++ app/src/main/java/com/alocar/frontend/BaseActivity.java
@@ -0,0 +1,70 @@
+package com.alocar.frontend;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+
+public class BaseActivity extends Activity {
+    boolean keyboardOn = false;
+    private ViewTreeObserver.OnGlobalLayoutListener keyboardLayoutListener = new KeyboardListener();
+
+    private boolean keyboardListenersAttached = false;
+    private ViewGroup rootLayout;
+
+    protected void onShowKeyboard(int keyboardHeight) {}
+    protected void onHideKeyboard() {}
+
+    protected void attachKeyboardListeners(View view) {
+        if (keyboardListenersAttached) {
+            return;
+        }
+
+        rootLayout = (ViewGroup) view;
+        rootLayout.getViewTreeObserver().addOnGlobalLayoutListener(keyboardLayoutListener);
+
+        keyboardListenersAttached = true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (keyboardListenersAttached) {
+            rootLayout.getViewTreeObserver().removeGlobalOnLayoutListener(keyboardLayoutListener);
+        }
+    }
+
+    private class KeyboardListener implements ViewTreeObserver.OnGlobalLayoutListener {
+        @Override
+        public void onGlobalLayout() {
+            int heightDiff = rootLayout.getRootView().getHeight() - rootLayout.getHeight();
+            int contentViewTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+
+            LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(BaseActivity.this);
+
+            if(heightDiff <= contentViewTop){
+                if (keyboardOn) {
+                    onHideKeyboard();
+
+                    Intent intent = new Intent("KeyboardWillHide");
+                    broadcastManager.sendBroadcast(intent);
+                    keyboardOn = false;
+                }
+            } else {
+                if (!keyboardOn) {
+                    int keyboardHeight = heightDiff - contentViewTop;
+                    onShowKeyboard(keyboardHeight);
+
+                    Intent intent = new Intent("KeyboardWillShow");
+                    intent.putExtra("KeyboardHeight", keyboardHeight);
+                    broadcastManager.sendBroadcast(intent);
+                    keyboardOn = true;
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/alocar/frontend/MainActivity.java
+++ app/src/main/java/com/alocar/frontend/MainActivity.java
@@ -1,21 +1,65 @@
 package com.alocar.frontend;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    ImageView logo;
+    ImageView banner;
+    TextView notMember;
+    TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logo);
+        banner = findViewById(R.id.banner);
+        notMember = findViewById(R.id.not_member);
+        signUp = findViewById(R.id.sign_up);
+        attachKeyboardListeners(findViewById(R.id.login_screen));
+    }
+
+    @Override
+    protected void onShowKeyboard(int keyboardHeight) {
+        logo.setVisibility(View.GONE);
+        banner.setVisibility(View.INVISIBLE);
+        notMember.setVisibility(View.GONE);
+        signUp.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onHideKeyboard() {
+        logo.setVisibility(View.VISIBLE);
+        banner.setVisibility(View.VISIBLE);
+        notMember.setVisibility(View.VISIBLE);
+        signUp.setVisibility(View.VISIBLE);
     }
 
     public void signUp(View view) {
         Intent signUpIntent = new Intent(this, SignUpPage1.class);
         startActivity(signUpIntent);
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+    }
+
+    public void showPassword(View view) {
+        ImageView eye = findViewById(R.id.visibility);
+        EditText password = findViewById(R.id.password);
+
+        if (password.getTransformationMethod() == PasswordTransformationMethod.getInstance()) {
+            eye.setImageDrawable(getDrawable(R.drawable.ic_visibility_off));
+            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+        } else {
+            eye.setImageDrawable(getDrawable(R.drawable.ic_visibility));
+            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        }
+        password.setSelection(password.getText().length());
     }
 }
